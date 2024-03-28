330925594fec8aaae5c442bb436bdf3416f22a0b
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-03-28 20:14:30 +0530

    Question 2

---
+++ Questions/question2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class question2 {
+    public static void main(String dj[]) {
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int a1 = a.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int a2 = a.nextInt();
+        System.out.print("Enter 3rd number: ");
+        int a3 = a.nextInt();
+        int sum = a1 + a2 + a3;
+        System.out.println("Sum of three number is: "+sum); 
+    }
+}
