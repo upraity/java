2a967a58a372f5c7f96ab8d9e068820e9e011def
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-03-30 21:17:05 +0530

    Question 4

---
+++ Questions/question4.java
@@ -0,0 +1,11 @@
+// Q.4 wap which ask the user to enter his/her name and greeta them with hello <name> have a good days text
+import java.util.*;
+public class question4 {
+    public static void main(String dj[]) {
+        Scanner n = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = n.next();
+        System.out.println("Hello " + name + " have a good day");
+
+    }
+}
