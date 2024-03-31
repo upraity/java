637d6e060781055ea2e2a248eb63fbb368b1816f
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-03-31 20:10:20 +0530

    Question 5

---
+++ Questions/question5.java
@@ -0,0 +1,13 @@
+//Q5. wap to convert kilometer to meter
+
+import java.util.*;
+public class question5 {
+    public static void main(String dj[]) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("Conert km to m");
+        System.out.print("Enter kilo meter to change meter: ");
+        float a = k.nextFloat();
+        float m = a * 1000;
+        System.out.println("In km: "+m);
+    }
+}
