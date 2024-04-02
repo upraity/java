ac6b96c56a470f96b9f2e5fbebe27a8b6bd93bad
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-02 23:46:05 +0530

    Question 7

---
+++ Questions/question7.java
@@ -0,0 +1,13 @@
+public class question7 {
+    public static void main(String[] dj) {
+        int y = 7;
+        int x = ++y + 8;
+        //find valuse of x = ?
+        System.out.println("Value of x: "+x); // 16
+
+        //for character 
+        char a = 'C';
+        a++;
+        System.out.println(a);
+    }
+}
