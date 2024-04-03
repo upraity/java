c5f58d0cde4e670556596a213750a571f5509f74
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-03 22:10:46 +0530

    Question 8

---
+++ Questions/question8.java
@@ -0,0 +1,10 @@
+public class question8 {
+    public static void main(String[] dj) {
+        float a = 7 / 4 * 9 / 2;
+        System.out.println(a); // it's gives 4 because it is int 
+
+        //when we wnt answer in float so write 
+        float b = 7 / 4.0f * 9 / 2.0f;
+        System.out.println(b); // it is in int
+    }
+}
