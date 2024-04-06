fdb36eb6441db88c4d2544f42f697d7f0bfd3ea0
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-06 19:51:35 +0530

    Question 11

---
+++ Questions/question11.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class question11 {
+    public static void main(String[] args) {
+        System.out.println("(v**2 - u**2)/2as");
+        System.out.println(7*49/7+35/7);
+    }
+}
