044b23dd6cf4f49da556d19ae66373ae73df8126
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-08 07:45:26 +0530

    Question 12

---
+++ Questions/question12.java
@@ -0,0 +1,10 @@
+/*
+ * Wap to convert a string to lowercase
+ */
+
+public class question12 {
+    public static void main(String[] dj) {
+        String name = "SabkaCode";
+        System.out.println(name.toLowerCase());
+    }
+}
