3caffc98346d48c2e8f88f6d44ee059ec88c2921
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-09 20:33:44 +0530

    Question 13

---
+++ Questions/question13.java
@@ -0,0 +1,10 @@
+/*
+ * Wap to replace space with underscore
+ */
+
+public class question13 {
+    public static void main(String[] dj) {
+        String para = "In this you can see the questions of java lang.";
+        System.out.println(para.replace(" ", "_"));
+    }
+}
