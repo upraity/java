4fe25dea90116b3adbf0127db61162cba929b535
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-12 22:45:18 +0530

    Question 16

---
+++ Questions/question16.java
@@ -0,0 +1,11 @@
+/*
+ * Wap to format the following letter using escape sequence characters.
+ * letter = "Dear SabkaCode, This is question of java."
+ */
+
+public class question16 {
+    public static void main(String[] dj) {
+        String letter = "\"Dear SabkaCode, This is question of java.\"";
+        System.out.println(letter);
+    }
+}
