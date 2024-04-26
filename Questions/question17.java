a7b5f459258335d9c406c84d98cd8bf5180adb2f
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-26 18:01:22 +0530

    Question 17

---
+++ Questions/question17.java
@@ -0,0 +1,12 @@
+/*
+ * Find the output og these code 
+ */
+public class question17 {
+    public static void main(String[] dj) {
+        int a = 10;
+        if (a == 11)
+            System.out.println("I am 11");
+        else
+            System.out.println("I am not 11");
+    }
+}
