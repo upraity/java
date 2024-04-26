0e7f4abc58b72610b71b63ae5c7e098e4413de61
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-26 18:03:35 +0530

    Associativity

---
+++ associativity.java
@@ -0,0 +1,12 @@
+/* Precedence & Associativity
+Highest precendence goes to * and /. Thery are then evaluted on the basis. or left to right
+
+*/
+public class associativity {
+    public static void main(String[] dj) {
+           int a = 6 * 5 - 19 / 2 ;
+           int b = 6 / 5 - 10 * 2;
+           System.out.println(a);
+           System.out.println(b);
+    }
+}
