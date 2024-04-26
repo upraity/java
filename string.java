e3de005d92f38567a56f847ae66bd4bab0d072e4
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-26 18:09:27 +0530

    String

---
+++ string.java
@@ -0,0 +1,18 @@
+/* Srting
+ * A string is a sequence of character
+ * String are immutable and cannot be changed
+ */
+
+import java.util.*;
+public class string {
+    public static void main(String[] dj) {
+        String st = new String("SabkaCode");
+        String s = "Dj";
+        System.out.println(st);
+        System.out.println(s);
+        int i = 5;
+        float f = 50.5f;
+        char c = 'r';
+        System.out.printf("value of i: %d, value of f: %f, value of c: %c",i,f,c); 
+    }
+}
